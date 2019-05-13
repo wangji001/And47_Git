233a4ef30ab5c5774ed7e80a846c72faed857033
wangji001 <giyoung001@gmail.com>
2019-05-13 11:10:22 +0900

    findViewById - textView

--- app/src/main/java/edu/android/and47_git/MainActivity.java
+++ app/src/main/java/edu/android/and47_git/MainActivity.java
@@ -6,10 +6,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
     }
 }
